1bf132bf9e5250ccbf48e52b9c9a7845baaea5aa
Leonardo Henrique Lopes Vieira <lhlv.dev@gmail.com>
2022-08-22 17:27:38 -0300

    Update script.js

--- WebClock/script.js
+++ WebClock/script.js
@@ -3,23 +3,23 @@ const minuteSpan = document.getElementById("minute");
 const secondSpan = document.getElementById("seconds");
 
 function pad2(number) { return (number < 10 ? '0' : '') + number }
+
 function changetime() 
 {
   const date = new Date();
   let hour = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-  
-  //mostrar valores no console da página
-  console.log("H:",hour," M:", minutes," S:", seconds);
 
-  //forçar exibição de 0 a frente dos retornos de um dígito 
+  //forçar exibição de 0 a frente dos retornos de apenas um dígito 
   hourSpan.textContent = pad2(hour);
   minuteSpan.textContent = pad2(minutes);
   secondSpan.textContent = pad2(seconds);
 
+  //mostrar valores no console da página
+  console.log("H:",hour," M:", minutes," S:", seconds);
+  
   //limpar o terminal
   if (seconds === 59) { console.clear(); }
 }
-
 setInterval(changetime, 1000);
